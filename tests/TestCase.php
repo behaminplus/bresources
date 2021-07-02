3d32b0f5d45a62add8c28bcd3df28098750ab83a
Omid Alizadeh <om_alizadeh@yahoo.com>
2021-07-02 20:27:44 +0430

    refactor: remove unused method

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,11 +7,6 @@ use Orchestra\Testbench\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-    }
-
     protected function getPackageProviders($app)
     {
         return [
