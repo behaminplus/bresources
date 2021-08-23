b74796dcb5fd36d635f5a590ac304ff15c47993e
Mohammad Hosein Abedini <mohammadhoseinabedini@gmail.com>
2021-08-23 10:08:12 +0430

    chore: do not use apiResponse() function in BasicRequest.php

--- src/Requests/BasicRequest.php
+++ src/Requests/BasicRequest.php
@@ -2,19 +2,19 @@
 
 namespace Behamin\BResources\Requests;
 
+use Behamin\BResources\Resources\BasicResource;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Http\Response as HttpResponse;
+use Illuminate\Http\Response;
 use Illuminate\Validation\ValidationException;
 
 class BasicRequest extends FormRequest
 {
     protected function failedValidation(Validator $validator)
     {
-        throw new ValidationException($validator, apiResponse()->status(HttpResponse::HTTP_UNPROCESSABLE_ENTITY)
-            ->errors(
-                $validator->errors()->first(),
-                $validator->errors()->toArray()
-            )->get(), $this->errorBag);
+        throw new ValidationException($validator, response(new BasicResource([
+            'error_message' => $validator->errors()->first(),
+            'errors' => $validator->errors()
+        ]), Response::HTTP_UNPROCESSABLE_ENTITY), $this->errorBag);
     }
 }
